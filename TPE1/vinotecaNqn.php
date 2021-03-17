94e49c299c69c1811462c28a89ef31d69cf4b01d
Matias <maty.infante@gmail.com>
2021-03-17 16:31:40 -0300

    initial commit

--- TPE1/vinotecaNqn.php
+++ TPE1/vinotecaNqn.php
@@ -1,26 +1,5 @@
 <?php
 
-function cargarArray()
-{
-    $vinoteca = array();
-
-    $vinoteca['Malbec'] = array(
-        array("cantidad" => 85, "anio" => "1999", "precio" => 240),
-        array("cantidad" => 64, "anio" => "2003", "precio" => 150)
-    );
-    $vinoteca['Merlot'] = array(
-        array("cantidad" => 82, "anio" => "2003", "precio" => 250),
-        array("cantidad" => 50, "anio" => "1975", "precio" => 1500)
-    );
-
-    $vinoteca['Cabernet Sauvignon'] = array(
-        array("cantidad" => 2, "anio" => "2008", "precio" => 270),
-        array("cantidad" => 54, "anio" => "2014", "precio" => 500)
-    );
-
-    return $vinoteca;
-}
-
 function calcStock($array)
 {
     $calcVinos = array();
